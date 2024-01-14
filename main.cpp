5368dbeeb8bd5ea2744abf2240f2e0a41c65f9da
velox0 <72274796+Velox@users.noreply.github.com>
2024-01-14 08:51:57 +0530

    put functions inside modules.hh

--- main.cpp
+++ main.cpp
@@ -1,22 +1,13 @@
+#include "modules.hh"
 #include <cstdlib>
 #include <iostream>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-static unsigned int max_length = 0;
-static unsigned short width = 0;
-static unsigned int lines = 1;
-static const bool is_ttyl = isatty(STDOUT_FILENO);
-
-/*
-  Iterates through the string character by character.
-  Calculates maximum line length.
-*/
-void scanarg(const char *str);
-
-void spacer();
-
-void display(const char *str);
+unsigned int max_length = 0;
+unsigned short width = 0;
+unsigned int lines = 1;
+const bool is_ttyl = isatty(STDOUT_FILENO);
 
 int main(int argc, char **argv) {
   if (argc == 1) {
@@ -57,43 +48,4 @@ int main(int argc, char **argv) {
 
   std::cout << "\n";
   return EXIT_SUCCESS;
-}
-
-void scanarg(const char *str) {
-  unsigned int str_length = 0;
-  for (int i = 0; *(str + i) != '\0'; i++) {
-    // count non-new-line characters
-    if (*(str + i) != '\n')
-      str_length++;
-
-    // count new-lines
-    else if (str_length > max_length) {
-      lines++;
-      max_length = str_length;
-      str_length = 0;
-    } else {
-      lines++;
-      str_length = 0;
-    }
-  }
-
-  if (str_length > max_length)
-    max_length = str_length;
-}
-
-void spacer() {
-  if (!is_ttyl || width < max_length)
-    return;
-  for (int spacer = 0; spacer < (width - max_length) / 2; spacer++)
-    std::cout << " ";
-}
-
-void display(const char *str) {
-  for (int j = 0; str[j] != '\0'; j++) {
-    std::cout << str[j];
-    if (str[j] == '\n')
-      spacer();
-  }
-  std::cout << std::endl;
-  spacer();
 }
---
+++ modules.hh
@@ -0,0 +1,62 @@
+#include <iostream>
+
+extern unsigned int max_length;
+extern unsigned short width;
+extern unsigned int lines;
+extern const bool is_ttyl;
+
+/*
+  Iterates through the string character by character.
+  Calculates maximum line length.
+*/
+
+void scanarg(const char *str);
+
+/*
+  adds horizontal space from left
+*/
+void spacer();
+
+/*
+  display string
+*/
+void display(const char *str);
+
+void scanarg(const char *str) {
+  unsigned int str_length = 0;
+  for (int i = 0; *(str + i) != '\0'; i++) {
+    // count non-new-line characters
+    if (*(str + i) != '\n')
+      str_length++;
+
+    // count new-lines
+    else if (str_length > max_length) {
+      lines++;
+      max_length = str_length;
+      str_length = 0;
+    } else {
+      lines++;
+      str_length = 0;
+    }
+  }
+
+  if (str_length > max_length)
+    max_length = str_length;
+}
+
+void spacer() {
+  if (!is_ttyl || width < max_length)
+    return;
+  for (int spacer = 0; spacer < (width - max_length) / 2; spacer++)
+    std::cout << " ";
+}
+
+void display(const char *str) {
+  for (int j = 0; str[j] != '\0'; j++) {
+    std::cout << str[j];
+    if (str[j] == '\n')
+      spacer();
+  }
+  std::cout << std::endl;
+  spacer();
+}
