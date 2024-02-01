df46f232cbb548dff4336f903e7cba07895c2c79
velox0 <72274796+Velox@users.noreply.github.com>
2024-02-01 23:32:14 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-g++ -O3 ./main.cpp -o /usr/bin/alinxt
+sudo g++ -O3 ./main.cpp -o /usr/bin/alinxt
 gzip -fk ./alinxt.6
 
 manpath="/usr/share/man/man6/"
 if [ ! -d "$manpath" ]; then
   # Create the path
-  mkdir -p "$manpath"
+  sudo mkdir -p "$manpath"
 fi
 
-mv ./alinxt.6.* $manpath
+sudo mv ./alinxt.6.* $manpath
